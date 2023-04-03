25009f73600ee05a373845ea19f17f840e343f64
Fitria Intani <nanditama.intani99@gmail.com>
2023-04-03 11:14:03 +0700

    rename db uri

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://intan:intan123@0.0.0.0:27017/eduwork-mongoose?authSource=admin'), {
+mongoose.connect(process.env.MONGO_URI), {
     useNewUrlParser: true,
     useUnifiedTopology: true
 };
